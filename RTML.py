6182bccc04b322a9face14789c2d1e5bdbf2a2b0
Priyadarshini Rajkumar <53154600+Priyadarshini-rajkumar@users.noreply.github.com>
2020-01-12 23:58:45 -0500

    Update RTML.py

--- RTML.py
+++ RTML.py
@@ -1,10 +1,12 @@
 #scheduling the targets
 import subprocess
 import pandas as pd 
+import numpy as np
 from   PyAstronomy  import pyasl
+from   datetime import datetime
 
-LT_targets_dir = 
-LT_targets = 
+LT_targets_dir = "/mnt/d/GROWTH/LT_algorithm/TargerList.csv"
+LT_targets =  pd.read_csv(LT_targets_dir)
 RA  = []
 DEC = []
 for i in np.arange(len(LT_targets)):  # ra and dec are converted from degrees to hms and dms respectively for sceduling purpose
@@ -16,30 +18,29 @@ for i in np.arange(len(LT_targets)):  # ra and dec are converted from degrees to
         DEC.append(s[1])   # list of all dec in sexagesimal
 LT_targets.loc[:,'RA']  = RA # storing the ra list as a column in LT_targets
 LT_targets.loc[:,'DEC'] = DEC  # storing the dec list as a column in LT_targets
-sceduling_targets       = LT_targets.loc[:,['Name','RA', 'DEC']]  # creating a new dataframe only with information required for sceduling
+sceduling_targets       = LT_targets.loc[:,['name','ra', 'dec']]  # creating a new dataframe only with information required for sceduling
 
 bin_dir      = "/home/extprajk/local_dev/rtml"
 docs_dir     = "/home/extprajk/local_dev/rtml/scripts/rtml_documents"
-project      = "gwem"
-username     = "copperwheat_chris"
-contactname  = "Copperwheat.Chris"
-password     = "WU2DGFmK"
+project      = "projectname"
+username     = "username"
+contactname  = "Contactname"
+password     = "password"
 raoff        = "0.0"
 decoff       = "0.0"
 startdate    = "2016-01-01T00:00:00"
 enddate      = "2017-12-31T00:00:00"
 exposure     = "60"
 nexpose      = "1"
-filt         = filter_choice
+filt         = "G"
 binning      = "2 2" 
 now_date     = str(datetime.time(datetime.now()))
 for i in np.arange(len(sceduling_targets)):
-     target   = '"' + str(sceduling_targets.loc[i,'Name']).strip()+ '"'
-     ra       = '"' + str(sceduling_targets.loc[i,'RA']).strip()  + '"'
-     dec      = '"' + str(sceduling_targets.loc[i,'DEC']).strip() + '"'
+     target   = '"' + str(sceduling_targets.loc[i,'name']).strip()+ '"' 
+     ra       = '"' + str(sceduling_targets.loc[i,'ra']).strip()  + '"'
+     dec      = '"' + str(sceduling_targets.loc[i,'dec']).strip() + '"'
      string   = bin_dir + "/create_rtml -rtml_version 3.1a -request -uid rtml://rtml-ioo-" + now_date + " -iauri file:/dev/null -project " + project + " -contact -contact_name " + contactname + " -contact_user " + username + " -observation -target -target_name " + target + " -ra " + ra + " -dec " + dec + " -ra_offset " + raoff + " -dec_offset " + decoff + " -start_date " + startdate + " -end_date " + enddate + " -device -device_name IO:O -device_type camera -device_spectral_region optical -device_filter " + filt + " -binning " + binning + " -exposure " + exposure + " s " + nexpose + " > " + docs_dir +"/ioo_rtml_document_" + str(i) + ".rtml"
      subprocess.call(string, shell = True)
      string2  = bin_dir + "/nodetester.tcsh -url http://161.72.57.3:8080/org_estar_nodeagent/services/NodeAgent -username " + username + " -password " + password + " -cookie -axis -urn  urn:/node_agent -method handle_rtml -rtml \"" + docs_dir +"/ioo_rtml_document_" + str(i) + ".rtml\""
      print (string2,'\n')
      subprocess.call(string2, shell = True)
-
